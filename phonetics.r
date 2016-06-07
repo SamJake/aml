a0dec7519ac595865a71b7d399ba9bc2050b03e2
Sam Jacob <Sam.Jacob@synechron.com>
2016-06-07 21:44:00 +0530

    aml | phonetics -v2

--- phonetics.r
+++ phonetics.r
@@ -18,10 +18,35 @@ names(input)
 
 
 name <- paste(input$FIRST_NAME[1],input$LAST_NAME[1])
-
 sdn_names <- data.frame(name=paste(sdn$firstName,sdn$lastName))
 
+#soundex
+s_name <- soundex(name, maxCodeLen = 14L)
+s_sdn_names <- soundex(sdn_names$name, maxCodeLen = 14L)
+
+#metaphone
+m_name <- metaphone(name,maxCodeLen = 14L)
+m_sdn_names <- metaphone(sdn_names$name, maxCodeLen =14L)
+
+#string_distances
+#direct
+dist <- stringdist(name,sdn_names$name)
+min(dist) 
+max(dist)
+
+#soundex
+
+s_dist <- stringdist(s_name,s_sdn_names)
+min(s_dist) 
+max(s_dist)
+
+
+#metaphone
+
+m_dist <- stringdist(m_name,m_sdn_names)
+min(m_dist)
+max(m_dist)
 
 
 
-phonetic(name,sdn_names)
+all <- data.frame(name=sdn_names,soundex=s_sdn_names,metaphone=m_sdn_names)
